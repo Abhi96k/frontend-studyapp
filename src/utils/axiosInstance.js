750b5404b8817f0cb277775c26c1254db18f566f
Abhishek Nangare <88939013+Abhi96k@users.noreply.github.com>
2024-08-05 23:38:41 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -19,10 +19,75 @@
 
 // export default axiosInstance;
 
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://backend-studyflick.onrender.com/api/v1",
+// });
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = localStorage.getItem("accessToken");
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+
+
+
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:8000/api/v1", // Your base URL here
+// });
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = localStorage.getItem("accessToken");
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://backend-studyflick.onrender.com/api/v1",
+// });
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = localStorage.getItem("accessToken");
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://backend-studyflick.onrender.com/api/v1",
+  baseURL: "https://backend-studyapp.onrender.com/api/v1",
 });
 
 axiosInstance.interceptors.request.use(
@@ -39,3 +104,4 @@ axiosInstance.interceptors.request.use(
 );
 
 export default axiosInstance;
+
